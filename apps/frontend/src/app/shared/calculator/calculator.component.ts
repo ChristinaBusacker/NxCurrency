3ce0037d747761286a66f6e750bd8399e0acc464
Christina Busacker <email@christina-busacker.de>
2024-04-23 11:12:07 +0200

    initial commit

---
+++ apps/frontend/src/app/shared/calculator/calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.scss',
+})
+export class CalculatorComponent {}
