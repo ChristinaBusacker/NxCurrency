687b3ce08a6c86fb5e6c0cf792a64e33a1ecfbc8
Christina Busacker <email@christina-busacker.de>
2024-04-23 13:20:26 +0200

    setup service and controller

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CurrencyModule } from './modules/currency/currency.module';
 
 @Module({
-  imports: [],
+  imports: [CurrencyModule],
   controllers: [AppController],
   providers: [AppService],
 })
