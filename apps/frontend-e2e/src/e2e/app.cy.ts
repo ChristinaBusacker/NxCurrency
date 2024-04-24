345069aa70d9883efb04c7cda856fff9fc4fe4f9
Christina Busacker <email@christina-busacker.de>
2024-04-24 17:45:39 +0200

    implementing frontend e2e tests

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,51 @@
-import { getGreeting } from '../support/app.po';
+describe('Currency Conversion', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="input-amount"]').as('inputAmount');
+    cy.get('[data-cy="input-currency"]').as('inputCurrency');
+    cy.get('[data-cy="output-amount"]').as('outputAmount');
+    cy.get('[data-cy="output-currency"]').as('outputCurrency');
+  });
 
-describe('frontend-e2e', () => {
-  beforeEach(() => cy.visit('/'));
+  const typeAndSelectCurrencies = (amount: string, inputCurrency: string, outputCurrency: string) => {
+    cy.get('@inputAmount').type(amount);
+    cy.get('@inputCurrency').select(inputCurrency);
+    cy.get('@outputCurrency').select(outputCurrency);
+  };
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('currency conversion form is visible', () => {
+    cy.get('@inputAmount').should('be.visible');
+    cy.get('@inputCurrency').should('be.visible');
+    cy.get('@outputAmount').should('be.visible');
+    cy.get('@outputCurrency').should('be.visible');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+  it('user can input values and select currencies for conversion', () => {
+    typeAndSelectCurrencies('1000', 'USD', 'EUR');
+    cy.get('@outputAmount').should('not.have.value', '0');
+  });
+
+  it('user can switch input and ouput values', () => {
+    typeAndSelectCurrencies('1000', 'USD', 'EUR');
+    cy.get('@outputAmount').invoke('val').then((value) => {
+      if (value) {
+        cy.get('[data-cy="inout-switch"]').click();
+        cy.get('@inputAmount').should('have.value', value);
+      } else {
+        throw new Error('output value was not valid');
+      }
+    });
+  });
+
+  it('displays the conversion history', () => {
+    typeAndSelectCurrencies('1000', 'USD', 'EUR');
+    cy.get('[data-cy="history-entry"]').should('have.length.at.least', 1);
+  });
+
+  it('clears the history when the clear button is clicked', () => {
+    typeAndSelectCurrencies('1000', 'USD', 'EUR');
+    cy.get('[data-cy="history-entry"]').should('have.length', 1);
+    cy.get('[data-cy="clear-history"]').click();
+    cy.get('[data-cy="history-entry"]').should('have.length', 0);
   });
 });
